08ace2a602239be42e4b10dbcd077e5d682ad882
fydne <serverafydne@gmail.com>
2021-04-09 16:04:01 +0500

    Added description of each action

--- example/EventHandlers.cs
+++ example/EventHandlers.cs
@@ -4,16 +4,16 @@ namespace example
 {
 	public class EventHandlers
 	{
-		private readonly Plugin plugin;
-		public EventHandlers(Plugin plugin) => this.plugin = plugin;
-		public void WaitingForPlayers()
+		private readonly Plugin plugin; /* <- plugin class(for accessing not static objects) */
+		public EventHandlers(Plugin plugin) => this.plugin = plugin; /* <- class declaration */
+		public void WaitingForPlayers() /* <- waiting for players */
 		{
-			GameObject.Find("StartRound").transform.localScale = Vector3.zero;
+			GameObject.Find("StartRound").transform.localScale = Vector3.zero; /* <- resizing the player waiting window(zero - 0) */
 		}
-		public void PlayerJoin(JoinEvent ev)
+		public void PlayerJoin(JoinEvent ev /* <- event */) /* <- the method called by the player entering the server */
 		{
-			string str = $"\n<color=#00fffb>Welcome</color>\n<color=#09ff00>Enjoy game!</color>";
-			ev.Player.ShowHint(str, 10);
+			string str = $"\n<color=#00fffb>Welcome</color>\n<color=#09ff00>Enjoy game!</color>"; /* <- text */
+			ev.Player.ShowHint(str, 10); /* <- shows hint with text for 10 sec */
 		}
 	}
 }
--- example/Plugin.cs
+++ example/Plugin.cs
@@ -1,30 +1,33 @@
 ﻿namespace example
 {
-    public class Plugin : Qurre.Plugin
+    public class Plugin : Qurre.Plugin /* <- indicate that the plugin inherits Qurre.Plugin */
     {
-        public EventHandlers EventHandlers;
+        public EventHandlers EventHandlers; /* <- declare a class with events, because it is not static */
         #region override
-        public override string Developer { get; } = "Qurre Team";
-        public override string Name { get; } = "Example Plugin";
-        public override System.Version Version { get; } = new System.Version(1, 0, 0);
-        public override System.Version NeededQurreVersion { get; } = new System.Version(1, 1, 1);
-        public override void Enable() => RegisterEvents();
-        public override void Disable() => UnregisterEvents();
+        /* variable names must match */
+        public override string Developer { get; } = "Qurre Team"; /* <- plugin developer(optional) */
+        public override string Name { get; } = "Example Plugin"; /* <- plugin name(optional) */
+        public override System.Version Version { get; } = new System.Version(1, 0, 0); /* <- plugin version(optional) */
+        public override System.Version NeededQurreVersion { get; } = new System.Version(1, 1, 1); /* <- needed version of qurre(optional) */
+        public override void Reload() => base.Reload(); /* <- method called by plugin reloading(optional) */
+        public override void Enable() => RegisterEvents(); /* <- method called by enabling the plugin */
+        public override void Disable() => UnregisterEvents(); /* <- method called by plugin disabling */
         #endregion
         #region RegEvents
+        /* registration of events(you can use any name) */
         private void RegisterEvents()
         {
-            EventHandlers = new EventHandlers(this);
-            Qurre.Events.Round.WaitingForPlayers += EventHandlers.WaitingForPlayers;
-            Qurre.Events.Player.Join += EventHandlers.PlayerJoin;
+            EventHandlers = new EventHandlers(this); /* <- class declaration with events */
+            Qurre.Events.Round.WaitingForPlayers += EventHandlers.WaitingForPlayers; /* <- method register */
+            Qurre.Events.Player.Join += EventHandlers.PlayerJoin; /* <- method register */
         }
         #endregion
         #region UnregEvents
         private void UnregisterEvents()
         {
-            Qurre.Events.Round.WaitingForPlayers -= EventHandlers.WaitingForPlayers;
-            Qurre.Events.Player.Join -= EventHandlers.PlayerJoin;
-            EventHandlers = null;
+            Qurre.Events.Round.WaitingForPlayers -= EventHandlers.WaitingForPlayers; /* <- method unregister */
+            Qurre.Events.Player.Join -= EventHandlers.PlayerJoin; /* <- method unregister */
+            EventHandlers = null; /* <- deleting a class with events */
         }
         #endregion
     }

38c2408ebfa9ad92612fcb7ee854fcc39bed1463
fydne <serverafydne@gmail.com>
2021-12-28 01:53:50 +0500

    heh

--- example/Config.cs
+++ example/Config.cs
@@ -5,8 +5,8 @@ namespace example
     {
         public string Name { get; set; } = "example-plugin";
         [System.ComponentModel.Description("yes, this is the description of the description")]
-        public string Description { get; } = "This is an example of creating a custom configuration for a plugin.";
+        public string Description { get; set; } = "This is an example of creating a custom configuration for a plugin.";
         [System.ComponentModel.Description("Welcome Text")]
-        public string Welcome { get; } = "\n<color=#00fffb>Welcome</color>\n<color=#09ff00>Enjoy game!</color>";
+        public string Welcome { get; set; } = "\n<color=#00fffb>Welcome</color>\n<color=#09ff00>Enjoy game!</color>";
     }
 }

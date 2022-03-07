df1e1544121adcd79cef09dcb6a2b595dccc3c51
Klarulor <55636688+Klarulor@users.noreply.github.com>
2022-03-07 21:25:28 +0200

    Naming conventsions are not.. Fixed

--- example/Plugin.cs
+++ example/Plugin.cs
@@ -10,17 +10,23 @@
         public override System.Version Version { get; } = new System.Version(1, 0, 0); /* <- plugin version(optional) */
         public override System.Version NeededQurreVersion { get; } = new System.Version(1, 11, 0); /* <- needed version of qurre(optional) */
         public override void Reload() => base.Reload(); /* <- method called by plugin reloading(optional) */
-        public override void Enable() => RegisterEvents(); /* <- method called by enabling the plugin */
-        public override void Disable() => UnregisterEvents(); /* <- method called by plugin disabling */
+        public override void Enable(){
+            CustomConfig = new Config();
+            CustomConfigs.Add(CustomConfig); /* <- Add and initialize the configuration */
+            EventHandlers = new EventHandlers(this); /* <- class declaration with events */
+            RegisterEvents();
+        } /* <- method called by enabling the plugin */
+        public override void Disable() {
+            UnregisterEvents();
+            EventHandlers = null; /* <- deleting a class with events */
+            CustomConfigs.Clear(); /* <- clear the list of configs */
+        } /* <- method called by plugin disabling */
         public Config CustomConfig { get; private set; } /* <- creating a new config class */
         #endregion
         #region RegEvents
         /* registration of events(you can use any name) */
         private void RegisterEvents()
         {
-            CustomConfig = new Config();
-            CustomConfigs.Add(CustomConfig); /* <- Add and initialize the configuration */
-            EventHandlers = new EventHandlers(this); /* <- class declaration with events */
             Qurre.Events.Round.Waiting += EventHandlers.WaitingForPlayers; /* <- method register */
             Qurre.Events.Player.Join += EventHandlers.PlayerJoin; /* <- method register */
         }
@@ -28,10 +34,8 @@
         #region UnregEvents
         private void UnregisterEvents()
         {
-            CustomConfigs.Clear(); /* <- clear the list of configs */
             Qurre.Events.Round.Waiting -= EventHandlers.WaitingForPlayers; /* <- method unregister */
             Qurre.Events.Player.Join -= EventHandlers.PlayerJoin; /* <- method unregister */
-            EventHandlers = null; /* <- deleting a class with events */
         }
         #endregion
     }
